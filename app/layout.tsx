0d46c9a77bf387a8edcee7683451c955ac276d81
jayrajshakha07 <jayrajshakha73@gmail.com>
2025-05-05 15:47:27 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type React from "react";
+import { Providers } from "@/provider/Provider";
+import "./globals.css";
+
+import { Inter, Merriweather } from "next/font/google";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-merriweather",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} ${merriweather.variable}`}>
+      <body className="font-sans">
+        <Providers>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
