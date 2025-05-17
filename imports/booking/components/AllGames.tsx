d17ed4c99c26f1c39e223e5c0881d54be5f6850e
jayrajshakha07 <jayrajshakha73@gmail.com>
2025-05-17 13:06:02 +0530

    fixed some issue

--- imports/booking/components/AllGames.tsx
+++ imports/booking/components/AllGames.tsx
@@ -12,14 +12,8 @@ import { useRouter } from "next/navigation";
 import { useBookingStore } from "@/store/bookingStore";
 
 const AllGames = () => {
-<<<<<<< Updated upstream
   const { games } = useBookingStore();
-  console.log(games, "games");
-=======
-  const { games, setGames } = useBookingStore();
-  const [loading, setLoading] = useState<boolean>(true);
 
->>>>>>> Stashed changes
   const router = useRouter();
 
   return (
